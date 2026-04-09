b9c8fcf1314044779190e7c9849f81c4cd281bbb
tygopal45 <jeegopal835@gmail.com>
2026-04-09 17:59:26 +0530

    pydantic o/p parser

---
+++ pydantic_output_parser.py
@@ -0,0 +1,41 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    model="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+class Person(BaseModel):
+
+    name: str = Field(description='Name of the person')
+    age: int = Field(gt=18, description='Age of the person')
+    city: str = Field(description='Name of the city the person belongs to')
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template='Generate the name, age and city of a fictional {place} person \n {format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+# prompt = template.invoke({'place':'Indian'})
+
+# result = model.invoke(prompt)
+
+# final_result = parser.parse(result.content)
+
+chain = template | model | parser
+
+final_result = chain.invoke({'place':'sri lankan'})
+
+print(final_result)
